1fc7d22c57d862414a444006f58edfd6ace034b4
Luigi <luigi@umbrellavalidator.com>
2020-05-27 20:59:42 +0000

    Add ethereum and atom

--- bot.js
+++ bot.js
@@ -2,9 +2,7 @@ const fetch = require('node-fetch');
 const { Client, MessageAttachment } = require('discord.js');
 const bot = new Client();
 
-require('./env');
 const botSecretToken = process.env.DISCORD_BOT_TOKEN;
-
 bot.login(botSecretToken);
 
 bot.on('ready', () => {
@@ -15,34 +13,70 @@ bot.on('message', msg => {
 	if (msg.content === 'ping') {
 		msg.reply('pong');		// Replies with "@speaker, msg"
 		msg.channel.send('pong');	// Replies with "msg"
-	} else if (msg.content.startsWith('fx')) {
-		console.log(msg.content);
-		const ticker = msg.content.split(' ')[1];
-		if (ticker === 'btc') {
-			const url = 'http://api.coincap.io/v2/assets/bitcoin';
-			getData(url, msg)
+	} else if (msg.content.startsWith('fx') || msg.content.startsWith('Fx')) {
+		// Log user input.
+		console.log(new Date, msg.content);
+		const ticker = msg.content.split(' ')[1].toLowerCase();
+		let url;
+		let isUnknownTicker = false;
+		switch(ticker) {
+			case 'btc':
+				url = 'http://api.coincap.io/v2/assets/bitcoin';
+				break;
+			case 'eth':
+				url = 'http://api.coincap.io/v2/assets/ethereum';
+				break;
+			case 'atom':
+				url = 'http://api.coincap.io/v2/assets/cosmos';
+				break;
+			default:
+				isUnknownTicker = true;
+				msg.reply(`I do not know what "${ticker}" is.`);
+				break;
+		}
+		if (!isUnknownTicker) {
+			// Async function IIFE is not needed if running with Deno
+			(async function () {
+				const d = await getData(url);
+				const p = getPrice(d);
+				const vwap = getVwap(d);
+
+				if ((ticker === 'btc') && (p > vwap)) {
+					// BTC rollercoaster up gif
+					const gif = 'https://media.giphy.com/media/7FBY7h5Psqd20/giphy.gif'
+
+					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+					msg.channel.send(new MessageAttachment(gif));
+				} else if (ticker === 'btc') {
+					// BTC rollercoaster down gif
+					const gif = 'https://media.giphy.com/media/RgxAkfVQWwkjS/giphy.gif';
+
+					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+					msg.channel.send(new MessageAttachment(gif));
+				} else {
+					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+				}
+			})();
 		}
 	}
 });
 
-async function getData(url, msg) {
+async function getData(url) {
 	const r = await fetch(url);
 	const j = await r.json();
-	let p = j.data.priceUsd;
-	let pvwap = j.data.vwap24Hr;
-	p = Math.floor(p);
-	pvwap = Math.floor(pvwap);
+	// Log when response returns.
+	console.log(new Date, j);
+	return j;
+}
 
-	const s = '$'+p+'/₿';
-	if (p > pvwap) {
-		// BTC rollercoaster up gif
-		const attachment = new MessageAttachment('https://media.giphy.com/media/7FBY7h5Psqd20/giphy.gif');
-		msg.channel.send(s);
-		msg.channel.send(attachment);
-	} else {
-		// BTC rollercoaster down gif
-		const attachment = new MessageAttachment('https://media.giphy.com/media/RgxAkfVQWwkjS/giphy.gif');
-		msg.channel.send(s);
-		msg.channel.send(attachment);
-	}
+function getPrice(j) {
+	return parseFloat(j.data.priceUsd).toFixed(2);
+}
+
+function getVwap(j) {
+	return parseFloat(j.data.vwap24Hr).toFixed(2);
+}
+
+function getMarketCap(j) {
+	return parseFloat(j.data.marketCapUsd).toFixed(2);
 }
