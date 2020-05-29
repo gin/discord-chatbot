11ea99fb16697be0c1d1dea7fde98722e4cf69df
Luigi <luigi@umbrellavalidator.com>
2020-05-29 06:24:31 +0000

    Add command to get storage usage info

--- bot.js
+++ bot.js
@@ -45,16 +45,16 @@ bot.on('message', msg => {
 					// BTC rollercoaster up gif
 					const gif = 'https://media.giphy.com/media/7FBY7h5Psqd20/giphy.gif'
 
-					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+					msg.channel.send(`**${ticker.toUpperCase()}:\n$${p}**`);
 					msg.channel.send(new MessageAttachment(gif));
 				} else if (ticker === 'btc') {
 					// BTC rollercoaster down gif
 					const gif = 'https://media.giphy.com/media/RgxAkfVQWwkjS/giphy.gif';
 
-					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+					msg.channel.send(`**${ticker.toUpperCase()}:\n$${p}**`);
 					msg.channel.send(new MessageAttachment(gif));
 				} else {
-					msg.channel.send(`${ticker.toUpperCase()}:\n$${p}`);
+					msg.channel.send(`**${ticker.toUpperCase()}:\n$${p}**`);
 				}
 			})();
 		}
@@ -74,22 +74,36 @@ bot.on('message', msg => {
 				let cpuLoad = os.loadavg();
 				function toPercent(n) { return (parseFloat(n)*100).toFixed(0); }
 				[cpuLoad[0], cpuLoad[1], cpuLoad[2]] = [toPercent(cpuLoad[0]), toPercent(cpuLoad[1]), toPercent(cpuLoad[2])];
-				s = `CPU (average load):\n1 min:\t${cpuLoad[0]}%\n5 min:\t${cpuLoad[1]}%\n15 min:\t${cpuLoad[2]}%`;
+				s = `**CPU (average load):\n\`\`\` 1 min:\t${cpuLoad[0]}%\n 5 min:\t${cpuLoad[1]}%\n15 min:\t${cpuLoad[2]}%\`\`\`**`;
 				msg.channel.send(s);
+				console.log(new Date, s);
 				break;
 			case 'mem':
 				const memFree = os.freemem();
 				const memTotal = os.totalmem();
-				const memPercent = (memFree/memTotal*100).toFixed(2);
+				const memPercent = ((memTotal - memFree)/memTotal*100).toFixed(2);
 
-				s = `Memory (free):\n${memPercent}% (${(memFree/1024/1024).toFixed(0)}MB)`;
+				s = `**Memory usage (free):**\n\`\`\`${memPercent}%\n(${(memFree/1024/1024).toFixed(0)} MB)\`\`\``;
 				msg.channel.send(s);
+				console.log(new Date, s);
+				break;
+			case 'hdd':
+				// Async function IIFE not needed in Deno.
+				(async function () {
+					const disk = require('diskusage');
+					const diskInfo = await disk.check('/');
+
+					function toGB(x) { return (x / (1024*1024*1024)).toFixed(1); }
+					const diskPercent = ((diskInfo.total - diskInfo.available) / diskInfo.total * 100).toFixed(0);
+
+					s = `**Storage usage (free):**\n\`\`\`${diskPercent}%\n(${toGB(diskInfo.available)}GB)\`\`\``;
+					msg.channel.send(s);
+					console.log(new Date, s);
+				})();
 				break;
 			default:
 				break;
 		}
-		// Log output.
-		console.log(new Date, s);
 	}
 });
 
